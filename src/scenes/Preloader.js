c78ac74342f7b06cd49d5f98c46f164d1932e23e
David Agra <davidagra@Davids-Mac-mini.lan>
2024-02-29 17:41:23 +0000

    remover loading bar

--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -10,16 +10,10 @@ export class Preloader extends Scene {
     this.add.image(660, 384, "background");
 
     //  A simple progress bar. This is the outline of the bar.
-    this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
 
     //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-    const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
 
     //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
-    this.load.on("progress", (progress) => {
-      //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
-      bar.width = 4 + 460 * progress;
-    });
   }
 
   preload() {
