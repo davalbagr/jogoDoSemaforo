3f716b310a067171d8ae40a0fb3c87cdd725f17c
davidagra <davalbagr2002@gmail.com>
2024-05-31 11:08:25 +0100

    final

--- server.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import {createServer} from "http";
-
-const app = express();
-app.use('/', express.static('dist'));
-const http = createServer(app);
-http.listen(8080);
